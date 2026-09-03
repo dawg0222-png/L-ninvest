d66bef118e9f41a880bf852d39d39d23069b6390
dawg0222-png <dawg0222@gmail.com>
2026-09-03 11:23:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'saferoad-lon-v21';
+const CACHE_NAME = 'saferoad-lon-v22';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
